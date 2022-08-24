5b7f8709280d38aa9babec6c66d7e783d4a05fb3
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-24 15:38:21 +0300

    fix: Fix html loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    MiniHtmlWebpackPlugin,
+    generateAttributes,
+    generateCSSReferences,
+    generateJSReferences
+} = require('mini-html-webpack-plugin');
 const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 
 module.exports = {
@@ -64,11 +69,70 @@ module.exports = {
         // clean: true,
     },
     plugins: [
-        new HtmlWebpackPlugin({
+        new MiniHtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html',
             chunks: ['index'],
-            // cache: false
+            context: {
+                title: 'Copy Site',
+                htmlAttributes: {
+                  lang: 'en'
+                },
+                cssAttributes: {
+                  rel: 'preload',
+                  as: 'style'
+                },
+                jsAttributes: {
+                  defer: true
+                }
+              },
+            template: ({
+                css,
+                js,
+                publicPath,
+                title,
+                htmlAttributes,
+                cssAttributes,
+                jsAttributes
+              }) => {
+                const htmlAttrs = generateAttributes(htmlAttributes);
+
+                const cssTags = generateCSSReferences({
+                  files: css,
+                  attributes: cssAttributes,
+                  publicPath
+                });
+
+                const jsTags = generateJSReferences({
+                  files: js,
+                  attributes: jsAttributes,
+                  publicPath
+                });
+
+                // TODO: You could push this to a separate file if you prefer
+                return `<!DOCTYPE html>
+                <html${htmlAttrs}>
+                  <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;600&display=swap" rel="stylesheet">
+                    <title>${title}</title>
+                    ${cssTags}
+                  </head>
+                  <body>
+                    <!-- Hero Section -->
+                    <section id="hero">
+                        <div class="max-w-6xl mx-auto text-center mb-40 px-10 pt-16">
+                        <!-- <img src="images/logo.svg" alt="" class="mx-auto my-16"> -->
+                        <svg class="mx-auto my-16"><use xlink:href="svg/spritesheet.svg#logo" /></svg>
+                        </div>
+                    </section>
+                    ${jsTags}
+                  </body>
+                </html>`;
+              }
         }),
         new SpriteLoaderPlugin()
     ]
